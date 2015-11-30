854e217eea0fee6d90998522a65d939324be19e5
fabian <fwirz@hsr.ch>
2015-11-30 22:25:20 +0100

    add ui function

---
+++ AutoReservation.Ui/Factory/RemoteDataAccessServiceFactory.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using AutoReservation.Common.Interfaces;
+using System.ServiceModel;
+
+namespace AutoReservation.Ui.Factory
+{
+    class RemoteDataAccessServiceFactory : IServiceFactory
+    {
+        public IAutoReservationService GetService()
+            {
+                ChannelFactory<IAutoReservationService> channelFactory = new ChannelFactory<IAutoReservationService>("AutoReservationService");
+                return channelFactory.CreateChannel();
+            }
+    }
+}
--- AutoReservation.Ui/ViewModels/KundeViewModel.cs
+++ AutoReservation.Ui/ViewModels/KundeViewModel.cs
@@ -4,11 +4,16 @@ using System.Windows.Input;
 using AutoReservation.Common.DataTransferObjects;
 using AutoReservation.Common.Extensions;
 using AutoReservation.Ui.Factory;
+using System.Collections.Generic;
+using System.Linq;
 
 namespace AutoReservation.Ui.ViewModels
 {
     public class KundeViewModel : ViewModelBase
     {
+        private readonly List<KundeDto> kundeOriginal = new List<KundeDto>();
+        private readonly ObservableCollection<KundeDto> kunden = new ObservableCollection<KundeDto>();
+
         public KundeViewModel(IServiceFactory factory) : base(factory)
         {
             
@@ -16,28 +21,53 @@ namespace AutoReservation.Ui.ViewModels
 
         public ObservableCollection<KundeDto> Kunden
         {
-            get { throw new NotImplementedException(); }
+            get { return kunden; }
         }
 
+        private KundeDto selectedKunde;
         public KundeDto SelectedKunde
         {
-            get { throw new NotImplementedException(); }
-            set { throw new NotImplementedException(); }
+            get { return selectedKunde; }
+            set
+            {
+                if (selectedKunde == value)
+                {
+                    return;
+                }
+                selectedKunde = value;
+                this.OnPropertyChanged(p => p.SelectedKunde);
+               
+            }
         }
 
 
         #region Load-Command
 
+        private RelayCommand loadCommand;
+
         public ICommand LoadCommand
         {
             get
             {
-                throw new NotImplementedException();
+                return loadCommand ?? (loadCommand = new RelayCommand(param => Load(), param => CanLoad()));
             }
+        }
+
+        private bool CanLoad()
+        {
+            return ServiceExists;
         }
 
         protected override void Load()
         {
+            Kunden.Clear();
+            kundeOriginal.Clear();
+            foreach (var kunde in Service.GetKunden())
+            {
+                Kunden.Add(kunde);
+                kundeOriginal.Add(kunde.Clone());
+            }
+            SelectedKunde = Kunden.FirstOrDefault();
             
         }
 
@@ -45,27 +75,70 @@ namespace AutoReservation.Ui.ViewModels
 
         #region Save-Command
 
+        private RelayCommand saveCommand;
+
         public ICommand SaveCommand
         {
             get
             {
-                throw new NotImplementedException();
+                return saveCommand ?? (saveCommand = new RelayCommand(param => SaveData(), param => CanSaveData()));
             }
         }
-       
+
+        private bool CanSaveData()
+        {
+            if (!ServiceExists)
+            {
+                return false;
+            }
+
+            return Validate(Kunden);
+        }
+
+        private void SaveData()
+        {
+            foreach (var kunde in Kunden)
+            {
+                if (kunde.Id == default(int))
+                {
+                    Service.AddKunde(kunde);
+                }
+                else
+                {
+                    var original = kundeOriginal.FirstOrDefault(ao => ao.Id == kunde.Id);
+                    Service.UpdateKunde(kunde, original);
+                }
+            }
+            Load();
+        }
+
         #endregion
 
         #region New-Command
+
+        private RelayCommand newCommand;
 
         public ICommand NewCommand
         {
             get
             {
-                throw new NotImplementedException();
+                return newCommand ?? (newCommand = new RelayCommand(param => New(), param => CanNew()));
             }
         }
 
+        private void New()
+        {
+            Kunden.Add(new KundeDto());
+        }
+
         #endregion
+
+        private bool CanNew()
+        {
+            return ServiceExists;
+        }
+
+        private RelayCommand deleteCommand;
 
         #region Delete-Command
 
@@ -73,9 +146,24 @@ namespace AutoReservation.Ui.ViewModels
         {
             get
             {
-                throw new NotImplementedException();
+                return deleteCommand ?? (deleteCommand = new RelayCommand(param => Delete(), param => CanDelete()));
             }
         }
+
+        private void Delete()
+        {
+            Service.DeleteKunde(SelectedKunde);
+            Load();
+        }
+
+        private bool CanDelete()
+        {
+            return
+                ServiceExists &&
+                SelectedKunde != null &&
+                SelectedKunde.Id != default(int);
+        }
+
 
         #endregion
     }
